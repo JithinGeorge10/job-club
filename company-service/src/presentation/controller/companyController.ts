fcc9ba86a7645da1ce5ec9b3cb87ba36322b6b22
JithinGeorge10 <jithin.george.el@gmail.com>
2024-11-22 18:13:43 +0530

    verified user in company controller

--- company-service/src/presentation/controller/companyController.ts
+++ company-service/src/presentation/controller/companyController.ts
@@ -12,10 +12,16 @@ export class CompanyController {
     constructor() {
         this.companyService = new CompanyService();
     }
-    async postJobController(req: Request, res: Response, next: NextFunction): Promise<void> {
+    async postJobController(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
         try {
-            const jobDetails = await this.companyService.addJob(req.body)
-            res.status(200).send({ jobDetails })
+            const userId = req.user?.user
+            if (userId) {
+                const jobDetails = await this.companyService.addJob(req.body)
+                res.status(200).send({ jobDetails })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
         } catch (error) {
             next(error)
         }
@@ -23,145 +29,224 @@ export class CompanyController {
 
     async getJobController(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
         try {
-            const jobDetails = await this.companyService.getJobDetails()
-            res.status(200).send({ jobDetails })
-        } catch (error) {
-            next(error)
-        }
-    }
-    async getsingleJobDetails(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const { jobId } = req.query
-            const jobDetails = await this.companyService.getSingleJobDetails(jobId)
-            res.status(200).send({ jobDetails })
-        } catch (error) {
-            next(error)
-        }
-    }
-    async submitApplication(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const submitApplication = await this.companyService.submitApplication(req.body)
-            res.status(200).send({ submitApplication })
-        } catch (error) {
-            next(error)
-        }
-    }
-    async changeStatus(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
+            const userId = req.user?.user
+            if (userId) {
+                const jobDetails = await this.companyService.getJobDetails()
+                res.status(200).send({ jobDetails })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
 
-            const changeStatus = await this.companyService.changeStatus(req.body)
-            res.status(200).send({ changeStatus })
         } catch (error) {
             next(error)
         }
     }
-    async getfilteredJobs(req: Request, res: Response, next: NextFunction): Promise<void> {
+    async getsingleJobDetails(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
         try {
-
-            const filteredJobs = await this.companyService.filteredJobs(req.body)
-            res.status(200).send({ filteredJobs })
+            const userId = req.user?.user
+            if (userId) {
+                const { jobId } = req.query
+                const jobDetails = await this.companyService.getSingleJobDetails(jobId)
+                res.status(200).send({ jobDetails })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
         } catch (error) {
             next(error)
         }
     }
-    async updateJobs(req: Request, res: Response, next: NextFunction): Promise<void> {
+    async submitApplication(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
         try {
-
-            const updatedJob = await this.companyService.editJob(req.body)
-            res.status(200).send({ updatedJob })
+            const userId = req.user?.user
+            if (userId) {
+                const submitApplication = await this.companyService.submitApplication(req.body)
+                res.status(200).send({ submitApplication })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
         } catch (error) {
             next(error)
         }
     }
-    async deleteJobs(req: Request, res: Response, next: NextFunction): Promise<void> {
+    async changeStatus(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
         try {
-
-            const { jobId } = req.body
-            const deleteJob = await this.companyService.deleteJob(jobId)
+            const userId = req.user?.user
+            if (userId) {
+                const changeStatus = await this.companyService.changeStatus(req.body)
+                res.status(200).send({ changeStatus })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
         } catch (error) {
             next(error)
         }
     }
-
-    async applicants(req: Request, res: Response, next: NextFunction): Promise<void> {
+    async getfilteredJobs(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
         try {
-            const { companyId } = req.query
-            const applicants = await this.companyService.applicants(companyId)
-            res.status(200).send({ applicants })
+            const userId = req.user?.user
+            if (userId) {
+                const filteredJobs = await this.companyService.filteredJobs(req.body)
+                res.status(200).send({ filteredJobs })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
         } catch (error) {
             next(error)
         }
     }
-
-    async changeStatusApplicant(req: Request, res: Response, next: NextFunction): Promise<void> {
+    async updateJobs(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
         try {
-            const { applicantId, status } = req.body
-            const applicants = await this.companyService.changeStatusApplicants(applicantId, status)
-            res.status(200).send({ applicants })
+            const userId = req.user?.user
+            if (userId) {
+                const updatedJob = await this.companyService.editJob(req.body)
+                res.status(200).send({ updatedJob })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
         } catch (error) {
             next(error)
         }
     }
-
-    async hiredCompanies(req: Request, res: Response, next: NextFunction): Promise<void> {
+    async deleteJobs(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
         try {
-            const { id } = req.query;
-            const hiredCompanies = await this.companyService.hiredCompanies(id)
-            res.status(200).send({ hiredCompanies })
-        } catch (error) {
-            next(error)
-        }
-    }
-    async rejectedCompanies(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const { id } = req.query;
-            const rejectedCompanies = await this.companyService.rejectedCompanies(id)
-            res.status(200).send({ rejectedCompanies })
-        } catch (error) {
-            next(error)
-        }
-    }
-    async inreviewCompanies(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const { id } = req.query;
-            const inreviewCompanies = await this.companyService.inreviewCompanies(id)
-            res.status(200).send({ inreviewCompanies })
-        } catch (error) {
-            next(error)
-        }
-    }
-    async interviewCompanies(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const { id } = req.query;
-            const interviewCompanies = await this.companyService.interviewCompanies(id)
-            res.status(200).send({ interviewCompanies })
-        } catch (error) {
-            next(error)
-        }
-    }
-    async companyDetails(req: Request, res: Response, next: NextFunction): Promise<void> {
-        try {
-            const { id } = req.query;
-            console.log(id)
-            const companyDetails = await this.companyService.companyDetails(id)
-            res.status(200).send({ companyDetails })
+            const userId = req.user?.user
+            if (userId) {
+                const { jobId } = req.body
+                const deleteJob = await this.companyService.deleteJob(jobId)
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
         } catch (error) {
             next(error)
         }
     }
 
-
-
-    async companyLogo(req: Request, res: Response, next: NextFunction): Promise<any> {
+    async applicants(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
         try {
-            console.log(req.body)
-            //    const { userId } = req.body;
-            //    const userIdFromToken = req.user?.user;
-            //     if (userId !== userIdFromToken) {
-            //         res.status(200).send({ success: false, message: 'Unauthorized: User ID does not match' });
-            //     }
-            const addProfileImage = await this.companyService.addProfileImage(req.body)
-            res.status(200).send({ addProfileImage })
+            const userId = req.user?.user
+            if (userId) {
+                const { companyId } = req.query
+                const applicants = await this.companyService.applicants(companyId)
+                res.status(200).send({ applicants })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async changeStatusApplicant(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const userId = req.user?.user
+            if (userId) {
+                const { applicantId, status } = req.body
+                const applicants = await this.companyService.changeStatusApplicants(applicantId, status)
+                res.status(200).send({ applicants })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async hiredCompanies(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const userId = req.user?.user
+            if (userId) {
+                const { id } = req.query;
+                const hiredCompanies = await this.companyService.hiredCompanies(id)
+                res.status(200).send({ hiredCompanies })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
+        } catch (error) {
+            next(error)
+        }
+    }
+    async rejectedCompanies(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const userId = req.user?.user
+            if (userId) {
+                const { id } = req.query;
+                const rejectedCompanies = await this.companyService.rejectedCompanies(id)
+                res.status(200).send({ rejectedCompanies })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
+        } catch (error) {
+            next(error)
+        }
+    }
+    async inreviewCompanies(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const userId = req.user?.user
+            if (userId) {
+                const { id } = req.query;
+                const inreviewCompanies = await this.companyService.inreviewCompanies(id)
+                res.status(200).send({ inreviewCompanies })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
+        } catch (error) {
+            next(error)
+        }
+    }
+    async interviewCompanies(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const userId = req.user?.user
+            if (userId) {
+                const { id } = req.query;
+                const interviewCompanies = await this.companyService.interviewCompanies(id)
+                res.status(200).send({ interviewCompanies })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
+        } catch (error) {
+            next(error)
+        }
+    }
+    async companyDetails(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const userId = req.user?.user
+            if (userId) {
+                const { id } = req.query;
+                console.log(id)
+                const companyDetails = await this.companyService.companyDetails(id)
+                res.status(200).send({ companyDetails })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async companyLogo(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<any> {
+        try {
+            const userId = req.user?.user
+            if (userId) {
+                const addProfileImage = await this.companyService.addProfileImage(req.body)
+                res.status(200).send({ addProfileImage })
+            } else {
+                console.error('User ID not found');
+                throw new Error('User ID is required to fetch doctor profile.');
+            }
         } catch (error) {
             next(error)
         }
--- company-service/src/presentation/routes/companyRoute.ts
+++ company-service/src/presentation/routes/companyRoute.ts
@@ -4,22 +4,22 @@ import {authenticateToken} from '../middleware/authenticateToken'
 const companyRoute = Router()
 const companyController = new CompanyController();
 
-companyRoute.post('/post-job', companyController.postJobController.bind(companyController));
+companyRoute.post('/post-job', authenticateToken,companyController.postJobController.bind(companyController));
 companyRoute.get('/get-jobDetails',authenticateToken,companyController.getJobController.bind(companyController));
-companyRoute.get('/get-singleJobDetails', companyController.getsingleJobDetails.bind(companyController));
-companyRoute.post('/submitApplication', companyController.submitApplication.bind(companyController));
-companyRoute.post('/changeStatus-jobDetails', companyController.changeStatus.bind(companyController));
-companyRoute.post('/get-filteredJobs', companyController.getfilteredJobs.bind(companyController));
-companyRoute.put('/update-jobDetails', companyController.updateJobs.bind(companyController));
-companyRoute.delete('/delete-jobDetails', companyController.deleteJobs.bind(companyController));
-companyRoute.get('/applicants', companyController.applicants.bind(companyController));
-companyRoute.patch('/changeStatus-Applicant', companyController.changeStatusApplicant.bind(companyController));
-companyRoute.get('/hiredCompanies', companyController.hiredCompanies.bind(companyController));
-companyRoute.get('/rejectedCompanies', companyController.rejectedCompanies.bind(companyController));
-companyRoute.get('/inreviewCompanies', companyController.inreviewCompanies.bind(companyController));
-companyRoute.get('/interviewCompanies', companyController.interviewCompanies.bind(companyController));
-companyRoute.get('/companyDetails', companyController.companyDetails.bind(companyController));
-companyRoute.post('/companyLogo', companyController.companyLogo.bind(companyController));
+companyRoute.get('/get-singleJobDetails',authenticateToken, companyController.getsingleJobDetails.bind(companyController));
+companyRoute.post('/submitApplication',authenticateToken, companyController.submitApplication.bind(companyController));
+companyRoute.post('/changeStatus-jobDetails',authenticateToken, companyController.changeStatus.bind(companyController));
+companyRoute.post('/get-filteredJobs', authenticateToken,companyController.getfilteredJobs.bind(companyController));
+companyRoute.put('/update-jobDetails',authenticateToken, companyController.updateJobs.bind(companyController));
+companyRoute.delete('/delete-jobDetails',authenticateToken, companyController.deleteJobs.bind(companyController));
+companyRoute.get('/applicants', authenticateToken,companyController.applicants.bind(companyController));
+companyRoute.patch('/changeStatus-Applicant',authenticateToken, companyController.changeStatusApplicant.bind(companyController));
+companyRoute.get('/hiredCompanies', authenticateToken,companyController.hiredCompanies.bind(companyController));
+companyRoute.get('/rejectedCompanies',authenticateToken, companyController.rejectedCompanies.bind(companyController));
+companyRoute.get('/inreviewCompanies',authenticateToken, companyController.inreviewCompanies.bind(companyController));
+companyRoute.get('/interviewCompanies', authenticateToken,companyController.interviewCompanies.bind(companyController));
+companyRoute.get('/companyDetails',authenticateToken, companyController.companyDetails.bind(companyController));
+companyRoute.post('/companyLogo',authenticateToken, companyController.companyLogo.bind(companyController));
 
 
 
