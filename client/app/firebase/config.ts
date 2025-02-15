115bb8eae186cbe00033c37ce4cde8fd68061e69
JithinGeorge10 <jithin.george.el@gmail.com>
2025-02-15 12:35:57 +0530

    firebaase changed

--- client/app/firebase/config.ts
+++ client/app/firebase/config.ts
@@ -1,17 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
-import { APIKEY,AUTHDOMAIN,PROJECT_ID,MESSAGINGSENDER_ID,APP_ID,MEASUREMENT_ID } from "@/utils/constants";
+// import { APIKEY,AUTHDOMAIN,PROJECT_ID,MESSAGINGSENDER_ID,APP_ID,MEASUREMENT_ID } from "@/utils/constants";
 import 'firebase/storage'
 
 
 const firebaseConfig = {
-    apiKey: APIKEY,
-    authDomain: AUTHDOMAIN,
-    projectId: PROJECT_ID,
+    apiKey: 'AIzaSyCdgdhyA8_JIc2bZsWgKROLYHnn0JcQm6k',
+    authDomain: 'job-club-5f5bb.firebaseapp.com',
+    projectId: 'job-club-5f5bb',
     storageBucket:'job-club-5f5bb.appspot.com',
-    messagingSenderId: MESSAGINGSENDER_ID,
-    appId: APP_ID,
-    measurementId:MEASUREMENT_ID 
+    messagingSenderId: '15783840151',
+    appId: '1:15783840151:web:3713e6cad3f756a4be9cd2',
+    measurementId:'G-PK7WH3YYCY'
 };
 
 export const app = initializeApp(firebaseConfig);
@@ -20,4 +20,13 @@ const auth = getAuth(app);
 export default auth
 
 
+
+
+// NEXT_PUBLIC_APIKEY=AIzaSyCdgdhyA8_JIc2bZsWgKROLYHnn0JcQm6k
+// NEXT_PUBLIC_AUTHDOMAIN=job-club-5f5bb.firebaseapp.com
+// NEXT_PUBLIC_PROJECT_ID=job-club-5f5bb
+// NEXT_PUBLIC_STORAGEBUCKET=job-club-5f5bb.appspot.com
+// NEXT_PUBLIC_MESSAGINGSENDER_ID=15783840151
+// NEXT_PUBLIC_APP_ID=1:15783840151:web:3713e6cad3f756a4be9cd2
+// NEXT_PUBLIC_MEASUREMENT_ID=G-PK7WH3YYCY
 
